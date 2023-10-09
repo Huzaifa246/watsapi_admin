94240333c537ce55c855d83740ab0b8ea51b5c5a
Huzaifa246 <huzaifa24680@gmail.com>
2023-10-09 22:05:30 +0500

    first commit

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -45,48 +45,9 @@ const Sidebar = ({ isSidebarOpen, toggleSidebar }) => {
                                 Users
                             </span>
                         </Nav.Link>
-                        <Nav.Link
-                            className={`mb-2 main-sidebar`}
-                            onClick={handleToggleSubMenu}
-                        >
-                            <FontAwesomeIcon icon={faUsers} className="me-2 sidebar-icon" />
-                            <span className='sidebar-text'>
-                                Traders
-                            </span>
-                            <FontAwesomeIcon icon={isTradersSubMenuOpen ? faAngleUp : faAngleDown} className="ml-auto submenu-icon" />
-                        </Nav.Link>
-                        <Nav className={`submenu ${isTradersSubMenuOpen ? 'submenu-open' : ''}`}>
-                            <Nav.Link href="/traders" className={`submenu-item ${window.location.pathname === '/traders' ? 'active-link' : ''}`}>
-                                Current Data
-                            </Nav.Link>
-                            <Nav.Link href="/pastTraders" className={`submenu-item ${window.location.pathname === '/pastTraders' ? 'active-link' : ''}`}>
-                                Past Data
-                            </Nav.Link>
-                        </Nav>
-
-                        <Nav.Link href="/investment" className={`mb-2 main-sidebar ${window.location.pathname === '/investment' ? 'active-link' : ''}`}>
-                            <FontAwesomeIcon icon={faMoneyBill} className="me-2 sidebar-icon" />
-                            <span className='sidebar-text'>
-                                Investment
-                            </span>
-                        </Nav.Link>
-                        <Nav.Link href="/withdraw" className={`mb-2 main-sidebar ${window.location.pathname === '/withdraw' ? 'active-link' : ''}`}>
-                            <FontAwesomeIcon icon={faMoneyBill} className="me-2 sidebar-icon" />
-                            <span className='sidebar-text'>
-                                WithDraw
-                            </span>
-                        </Nav.Link>
                     </ul>
                 </div>
             </nav>
-            {/* <div
-                className={`sidebar-toggle sidebar-togglestyle ${isToggleActive ? 'active' : ''}`}
-                onClick={handleToggle}
-            >
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-grid" viewBox="0 0 16 16" className='grid-icon-style'>
-                    <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zM2.5 2a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zM1 10.5A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3z" />
-                </svg>
-            </div> */}
         </>
     );
 };
